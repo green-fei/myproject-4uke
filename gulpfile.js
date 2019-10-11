2764448fec14967352f3d6ce50ce33688080071c
feifei <1085003979@qq.com>
2019-10-11 22:08:52 +0800

    第一版

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// 请求gulp
+const gulp=require("gulp");
+const sass=require('gulp-sass');
+// 执行监听任务
+//复制项目下的所有代码复制到服务器下的发布目录下
+    gulp.task('copyfile',async ()=>{
+        gulp.src('./**/*')
+        .pipe(gulp.dest('C:\\phpStudy\\WWW\\publish'));
+    })
+// 监听文件，当开发文件有改动的时候，就会自动保存到www
+    gulp.task('copyfile',async ()=>{
+        gulp.watch('*.html',async ()=>{
+        gulp.src('*.html')
+        ,pipe(sass())
+        .pipe(gulp.dest('C:\\phpStudy\\WWW\\publish'));
+    });
+})
